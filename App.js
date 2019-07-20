65c4a93900761c8379ea0ccbe713a862cffa56ee
Mikael Sand <msand@abo.fi>
2019-07-21 01:03:33 +0300

    Add static comparison

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Dimensions, Animated } from 'react-native';
+import { StyleSheet, View, Dimensions, Animated, Platform } from 'react-native';
 import { Svg, Text, TSpan, G } from 'react-native-svg';
 
 const { width, height } = Dimensions.get('window');
 const AnimatedText = Animated.createAnimatedComponent(Text);
+const tinyWeight = Platform.OS === 'ios' ? 450 : 60;
 
 function getInitialState() {
   const anim = new Animated.Value(0);
@@ -30,21 +31,31 @@ export default class App extends Component {
     const { oneTo1E3 } = this.state;
     return (
       <View style={styles.container}>
-        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="0 0 700 700">
+        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="40 0 700 700">
           <Text fontSize={30}>
-            <TSpan x={100} y={100}>System font</TSpan>
-            <TSpan x={100} y={300}>IBMPlexSansVar</TSpan>
-            <TSpan x={100} y={500}>PublicSans-Thin_Regular</TSpan>
-            <TSpan x={100} y={700}>TINY5x3</TSpan>
+            <TSpan x={50} y={100}>System font</TSpan>
+            <TSpan x={50} y={300}>IBMPlexSansVar</TSpan>
+            <TSpan x={50} y={500}>PublicSans-Thin_Regular</TSpan>
+            <TSpan x={50} y={700}>TINY5x3</TSpan>
           </Text>
         </Svg>
-        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="0 0 700 700">
+        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="30 0 700 700">
+          <G fontSize={90}>
+            <Text fontWeight={100}>
+              <TSpan x={50} y={200}>Testing</TSpan>
+              <TSpan x={50} y={400} fontFamily="IBMPlexSansVar">Testing</TSpan>
+              <TSpan x={50} y={600} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
+              <TSpan x={50} y={800} fontFamily="TINY5x3" fontWeight={tinyWeight}>Testing</TSpan>
+            </Text>
+          </G>
+        </Svg>
+        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="-300 0 700 700">
           <G fontSize={90}>
             <AnimatedText fontWeight={oneTo1E3}>
-              <TSpan x={100} y={200}>Testing</TSpan>
-              <TSpan x={100} y={400} fontFamily="IBMPlexSansVar">Testing</TSpan>
-              <TSpan x={100} y={600} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
-              <TSpan x={100} y={800} fontFamily="TINY5x3">Testing</TSpan>
+              <TSpan x={50} y={200}>Testing</TSpan>
+              <TSpan x={50} y={400} fontFamily="IBMPlexSansVar">Testing</TSpan>
+              <TSpan x={50} y={600} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
+              <TSpan x={50} y={800} fontFamily="TINY5x3">Testing</TSpan>
             </AnimatedText>
           </G>
         </Svg>
