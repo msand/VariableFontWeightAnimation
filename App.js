a342d3d610a03aee5f453c6d4e99596d26efd3d7
Mikael Sand <msand@abo.fi>
2019-07-20 22:31:41 +0300

    Variable font weight animation example

--- App.js
+++ App.js
@@ -1,114 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, View, Dimensions, Animated } from 'react-native';
+import { Svg, Text, TSpan } from 'react-native-svg';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const { width, height } = Dimensions.get('window');
+const AnimatedText = Animated.createAnimatedComponent(Text);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function getInitialState() {
+  const anim = new Animated.Value(0);
+  const oneTo1E3 = anim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 1E3],
+  });
+  return { anim, oneTo1E3 };
+}
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+export default class App extends Component {
+  state = getInitialState();
+
+  componentDidMount() {
+    const { anim } = this.state;
+    Animated.timing(anim, {
+      toValue: 1,
+      duration: 15E3,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  render() {
+    const { oneTo1E3 } = this.state;
+    return (
+      <View style={styles.container}>
+        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="0 0 70 70">
+          <Text fontSize={3}>
+            <TSpan x={10} y={10}>System font</TSpan>
+            <TSpan x={10} y={30}>IBMPlexSansVar</TSpan>
+            <TSpan x={10} y={50}>PublicSans-Thin_Regular</TSpan>
+            <TSpan x={10} y={70}>TINY5x3</TSpan>
+          </Text>
+        </Svg>
+        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="0 0 70 70">
+          <AnimatedText fontWeight={oneTo1E3}>
+            <TSpan x={10} y={20}>Testing</TSpan>
+            <TSpan x={10} y={40} fontFamily="IBMPlexSansVar">Testing</TSpan>
+            <TSpan x={10} y={60} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
+            <TSpan x={10} y={80} fontFamily="TINY5x3">Testing</TSpan>
+          </AnimatedText>
+        </Svg>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  container: {
+    backgroundColor: '#ecf0f1',
   },
 });
-
-export default App;
