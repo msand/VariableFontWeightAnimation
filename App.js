d8afcd8a1d5efff7402362c974f768ed5a3a0000
Mikael Sand <msand@abo.fi>
2019-07-21 14:55:00 +0300

    [optimize] Split logic into PureComponents and isolate state changes

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ function getInitialState() {
   return { anim, oneTo1E3, TINY5x3 };
 }
 
-export default class App extends PureComponent {
+class AnimatedFontWeight extends PureComponent {
   state = getInitialState();
 
   componentDidMount() {
@@ -31,51 +31,79 @@ export default class App extends PureComponent {
     }).start();
   }
 
+  render() {
+    const { oneTo1E3, TINY5x3 } = this.state;
+    return (
+      <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="-350 -30 700 700">
+        <G transform="translate(0, 100)" fontSize={90}>
+          <AnimatedText fontWeight={oneTo1E3}>
+            <TSpan x={0} y={0}>Testing</TSpan>
+            <TSpan x={0} y={200} fontFamily="IBMPlexSansVar">Testing</TSpan>
+            <TSpan x={0} y={400} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
+          </AnimatedText>
+          <AnimatedText fontWeight={TINY5x3}>
+            <TSpan x={0} y={600} fontFamily="TINY5x3">Testing</TSpan>
+          </AnimatedText>
+        </G>
+      </Svg>
+    );
+  }
+}
+
+class VariableFontWeight extends PureComponent {
+  render() {
+    const { value } = this.props;
+    return (
+      <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="-10 -30 700 700">
+        <Text fontSize={30}>
+          <TSpan x={0} y={0}>System font</TSpan>
+          <TSpan x={0} y={200}>IBMPlexSansVar</TSpan>
+          <TSpan x={0} y={400}>PublicSans-Thin_Regular</TSpan>
+          <TSpan x={0} y={600}>TINY5x3</TSpan>
+        </Text>
+        <G transform="translate(10, 100)">
+          <Text fontSize={90} fontWeight={value || 100}>
+            <TSpan x={0} y={0}>Testing</TSpan>
+            <TSpan x={0} y={200} fontFamily="IBMPlexSansVar">Testing</TSpan>
+            <TSpan x={0} y={400} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
+            <TSpan x={0} y={600} fontFamily="TINY5x3" fontWeight={value || 60}>Testing</TSpan>
+          </Text>
+        </G>
+      </Svg>
+    );
+  }
+}
+
+class FontWeightSlider extends PureComponent {
+  render() {
+    return (
+      <Slider
+        minimumValue={1}
+        maximumValue={1000}
+        style={styles.slider}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+        onValueChange={this.props.onValueChange}
+      />
+    );
+  }
+}
+
+export default class App extends PureComponent {
+  state = {};
+
   onValueChange = value => this.setState({ value });
 
   render() {
-    const { oneTo1E3, TINY5x3, value } = this.state;
+    const { value } = this.state;
     return (
       <View style={styles.container}>
-        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="-10 -30 700 700">
-          <Text fontSize={30}>
-            <TSpan x={0} y={0}>System font</TSpan>
-            <TSpan x={0} y={200}>IBMPlexSansVar</TSpan>
-            <TSpan x={0} y={400}>PublicSans-Thin_Regular</TSpan>
-            <TSpan x={0} y={600}>TINY5x3</TSpan>
-          </Text>
-          <G transform="translate(10, 100)">
-            <Text fontSize={90} fontWeight={value || 100}>
-              <TSpan x={0} y={0}>Testing</TSpan>
-              <TSpan x={0} y={200} fontFamily="IBMPlexSansVar">Testing</TSpan>
-              <TSpan x={0} y={400} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
-              <TSpan x={0} y={600} fontFamily="TINY5x3" fontWeight={value || 60}>Testing</TSpan>
-            </Text>
-          </G>
-        </Svg>
-        <Svg width={width} height={height} style={StyleSheet.absoluteFill} viewBox="-350 -30 700 700">
-          <G transform="translate(0, 100)" fontSize={90}>
-            <AnimatedText fontWeight={oneTo1E3}>
-              <TSpan x={0} y={0}>Testing</TSpan>
-              <TSpan x={0} y={200} fontFamily="IBMPlexSansVar">Testing</TSpan>
-              <TSpan x={0} y={400} fontFamily="PublicSans-Thin_Regular">Testing</TSpan>
-            </AnimatedText>
-            <AnimatedText fontWeight={TINY5x3}>
-              <TSpan x={0} y={600} fontFamily="TINY5x3">Testing</TSpan>
-            </AnimatedText>
-          </G>
-        </Svg>
+        <VariableFontWeight value={value} />
+        <AnimatedFontWeight />
         <View style={styles.sliderView}>
-          <Slider
-            style={styles.slider}
-            minimumValue={1}
-            maximumValue={1000}
-            minimumTrackTintColor="#00FFFF"
-            maximumTrackTintColor="#000000"
-            onValueChange={this.onValueChange}
-          />
+          <FontWeightSlider onValueChange={this.onValueChange} />
           <NativeText>
-            {this.state.value && +this.state.value.toFixed(3) || ''}
+            {value && +value.toFixed(3) || ''}
           </NativeText>
         </View>
       </View>
@@ -89,8 +117,8 @@ const styles = StyleSheet.create({
   },
   sliderView: {
     padding: '10%',
-    height: '100%',
     width: '100%',
+    height: 120,
   },
   slider: {
     alignSelf: 'center',
