7d87235c5d38e01a37bd3592e794acfa3e235b93
Mikael Sand <msand@abo.fi>
2019-07-25 16:35:16 +0300

    Add jest snapshot

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -12,3 +12,8 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+test('snapshot matches', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
